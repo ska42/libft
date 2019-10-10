4e41e44240dab1cfddc8600b0d1f0e65639c6fb9
Leo MARTIN <lmartin@e1r2p2.42.fr>
2019-10-10 13:30:49 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 08:58:10 by lmartin           #+#    #+#             */
-/*   Updated: 2019/10/09 18:40:22 by lmartin          ###   ########.fr       */
+/*   Updated: 2019/10/10 13:29:38 by lmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	int		i;
 	char	*pt;
 
-	i = -1;
+	i = 0;
+	while (s[i] && i < (int) start)
+		i++;
+	if (i < start)
+		return (0);
 	if (!(pt = malloc((len + 1) * sizeof(char))))
 		return (0);
-	while (i++ < (int)len - 1)
+	i = -1;
+	while (++i < (int)len)
 		pt[i] = s[start + i];
 	pt[i] = '\0';
 	return (pt);
